0327347088d243a66aaa25b3b957b539ed0d20b7
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-09-12 22:03:11 -0300

    Correções e Uploads
    - Corrigido Código do gulpfile.js;
    - Upload Fontes;
    - Upload Imagens;
    - Criado index.html;

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,26 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src('./src/styles*.scss')
+    return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/styles/css'));
+    .pipe(gulp.dest('./dist/styles'));
 }
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(gulp.dest('./dist/js/'));
 }
 
 function images() {
     return gulp.src('./src/images/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
+    .pipe(gulp.dest('./dist/images/'));
 }
 
 exports.default = gulp.parallel(styles, scripts, images);
 
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss'), gulp.parallel(styles);
-    gulp.watch('./src/scripts/*.js'), gulp.parallel(scripts);
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
