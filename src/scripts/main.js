4c969fdeaf857e7e67ecd424b8b48c30f3c34eb8
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-09-15 03:09:30 -0300

    Criado Código Javascript e Display Responsivo
    - Criado Código Para Esconder o Header;
    - Configurado Display Responsivo;

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
     for (let i = 0; i < questions.length;i ++) {
         questions[i].addEventListener('click', openAndClose);
     }
+
+    window.addEventListener('scroll', function() {
+        const scrollHeight = window.scrollY;
+
+        if (scrollHeight < heroHeight) {
+            hideHeader();
+        } else {
+            showHeader();
+        }
+    })
 })
 
 function openAndClose(element) {
@@ -11,4 +24,14 @@ function openAndClose(element) {
     const parentElement = element.target.parentNode;
 
     parentElement.classList.toggle(nameOfClass);
+}
+
+function hideHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
